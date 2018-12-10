103528d337ca40ac64efe23b857006bba3f7487e
wytzz <wytzeaz@hotmail.com>
2018-12-10 23:45:48 +0100

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Wytze Dijkstra on 10/12/2018.
+//  Copyright © 2018 Wytze Dijkstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
